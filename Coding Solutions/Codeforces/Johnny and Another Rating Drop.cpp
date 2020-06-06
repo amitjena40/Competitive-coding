de831d21decb9afb83e1e0688a2f0cf7a3dc6d8b
amitjena40 <amitjena40@gmail.com>
2020-06-06 20:52:53 +0530

    CF647

---
+++ Coding Solutions/Codeforces/Johnny and Another Rating Drop.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+#define PB push_back
+
+using namespace std;
+
+void solve()
+{
+	ll n;
+	cin >> n;
+	ll ans = 2 * n - __builtin_popcountll(n);
+	cout << ans << "\n";
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
