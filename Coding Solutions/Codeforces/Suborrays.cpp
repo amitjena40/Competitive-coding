60bbe33df1a065d00fd5e3620228a0de91980ceb
amitjena40 <amitjena40@gmail.com>
2020-08-09 23:29:25 +0530

    CF663

---
+++ Coding Solutions/Codeforces/Suborrays.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+#define PB push_back
+
+using namespace std;
+
+void solve()
+{
+	ll n;
+	cin >> n;
+	for (ll i = 1; i <= n; i++)
+	{
+		cout << i << " ";
+	}
+	cout << "\n";
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
